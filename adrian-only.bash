801a7c106eeee237969cde57a1f3c7d866559e1a
aidswidjaja <adrian.marcus.widjaja@gmail.com>
2021-05-11 14:07:32 +1000

    Fix bash script

---
+++ adrian-only.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "INFO: This script only likes Adrian"
+~/git/PyongyangRacer/SeaMonkey.app/Contents/MacOS/seamonkey-bin --profile ~/git/PyongyangRacer/Profile
--- pyongyangracer.bash
+++ pyongyangracer.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "INFO: For this script to run, the Pyongyang Racer directory MUST be in your Downloads folder."
-~/Downloads/PyongyangRacer/SeaMonkey.app/Contents/MacOS/seamonkey-bin --profile ~/Downloads/PyongyangRacer/Profile
+~/Downloads/pyongyangracer-main/SeaMonkey.app/Contents/MacOS/seamonkey-bin --profile ~/Downloads/pyongyangracer-main/Profile
